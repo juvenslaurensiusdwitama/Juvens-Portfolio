3a4c4ab7e0ea71d23b548e50823c97c267531457
juvenslaurensiusdwitama <juvenslaurensiusdwitama@gmail.com>
2026-06-10 18:23:25 +0700

    testing: google font

--- portfolio-project/src/sections/Header.jsx
+++ portfolio-project/src/sections/Header.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-around items-center font-display'>
+      <h1 className='text-[24px]'>Juvens.dev</h1>
+      <ul className='flex gap-8 text-[18px]'>
+        <li className=''>About</li>
+        <li className=''>Experiences</li>
+        <li className=''>Projects</li>
+        <li className=''>Skills</li>
+      </ul>
+      <button className='flex gap-8 text-[18px]'>Resume</button>
+    </div>
   )
 }
 
