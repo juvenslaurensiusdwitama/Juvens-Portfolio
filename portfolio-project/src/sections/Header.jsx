bd4764c45ee14232335277949f5df54bd2561b9f
juvenslaurensiusdwitama <juvenslaurensiusdwitama@gmail.com>
2026-06-12 14:55:13 +0700

    feat: hero

--- portfolio-project/src/sections/Header.jsx
+++ portfolio-project/src/sections/Header.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import download from "../assets/download.png"
 
 const Header = () => {
   return (
-    <div className='flex justify-around items-center font-display'>
-      <h1 className='text-[24px]'>Juvens.dev</h1>
-      <ul className='flex gap-8 text-[18px]'>
+    <div className='flex justify-around items-center py-4 fixed top-0 left-0 right-0'>
+      <h1 className='text-2xl font-[600]'>Juvens.dev</h1>
+      <ul className='flex gap-8 text-xl font-[500]'>
         <li className=''>About</li>
         <li className=''>Experiences</li>
         <li className=''>Projects</li>
         <li className=''>Skills</li>
       </ul>
-      <button className='flex gap-8 text-[18px]'>Resume</button>
+      <button className='flex items-center gap-2 bg-black px-4 py-2 rounded-md'>
+        <p className='text-lg font-[500] text-white'>Resume</p>
+        <img src={download} alt="" className='h-[14px] font-[500]'/>
+      </button>
     </div>
   )
 }
