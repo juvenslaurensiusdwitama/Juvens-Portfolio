6def763ccda20b2db85570e3134921beab06d564
juvenslaurensiusdwitama <juvenslaurensiusdwitama@gmail.com>
2026-06-22 12:22:05 +0700

    .

--- portfolio-project/src/sections/Skills.jsx
+++ portfolio-project/src/sections/Skills.jsx
@@ -1,9 +1,89 @@
+import Skill from '@/components/Skill'
 import React from 'react'
 
 const Skills = () => {
+
+  const datas = [
+    {
+      icon: null,
+      name: 'HTML',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'CSS',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'JavaScript',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'React JS',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'Tailwind CSS',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'Shadcn UI',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'TypeScript',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'Next JS',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'Firebase',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'Postman API',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'GIT',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'GitHub',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'Vite',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'Vercel',
+      type: '',
+    },
+    {
+      icon: null,
+      name: 'Figma',
+      type: '',
+    },
+  ]
+
   return (
     <div className='flex justify-center items-center h-dvh'>
-      <h1>Hard Skills</h1>
+      <Skill/>
     </div>
   )
 }
