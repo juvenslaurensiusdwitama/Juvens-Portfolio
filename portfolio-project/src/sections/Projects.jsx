8f7bc0fc24752eb3be14121a958fb4fc396ecfb8
juvenslaurensiusdwitama <juvenslaurensiusdwitama@gmail.com>
2026-07-02 12:03:52 +0700

    '.'

--- portfolio-project/src/sections/Projects.jsx
+++ portfolio-project/src/sections/Projects.jsx
@@ -57,7 +57,7 @@ const Projects = () => {
       className='flex flex-col items-center gap-4 lg:pt-23 sm:pt-18 pt-4.5' 
       id='projects'
     >
-      <h1 className='xl:text-3xl lg:text-2xl sm:text-xl text-white font-semibold'>Past Projects</h1>
+      <h1 className='xl:text-3xl lg:text-2xl sm:text-xl text-lg text-white font-semibold'>Past Projects</h1>
       <div className='w-full xl:max-w-[1300px] xl:px-[50px] lg:max-w-[910px] md:max-w-[700px] sm:max-w-[580px] max-w-[302px] grid grid-cols-1 lg:grid-cols-2 sm:gap-0 gap-4'>
         {datas.toReversed().map((data, index) => (
           <Project key={index} data={data} />
