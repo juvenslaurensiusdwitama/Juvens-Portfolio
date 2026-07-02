5f0d1be27554699d2c0abc12f91003b839ba2ec9
juvenslaurensiusdwitama <juvenslaurensiusdwitama@gmail.com>
2026-07-02 11:58:28 +0700

    '.'

--- portfolio-project/src/sections/Header.jsx
+++ portfolio-project/src/sections/Header.jsx
@@ -23,8 +23,10 @@ const Header = () => {
   return (
     <>
       <div className="flex justify-center fixed left-0 right-0 top-0 z-50">
-        <div className='flex text-white justify-between items-center w-full xl:max-w-[1300px] xl:px-[50px] lg:max-w-[910px] md:max-w-[700px] sm:max-w-[580px] max-w-[308px] mt-7'>
-          <h1 className='xl:text-2xl md:text-xl sm:text-lg text-base font-[600] cursor-pointer' onClick={() => scrollToSection('introduction')}>Juvens.dev</h1>
+        <div className='flex text-white justify-between items-center w-full xl:max-w-[1300px] xl:px-[50px] lg:max-w-[910px] md:max-w-[700px] sm:max-w-[580px] max-w-[302px] sm:mt-7 mt-5'>
+          <h1 className='xl:text-2xl md:text-xl sm:text-lg text-base font-[600] cursor-pointer' onClick={() => scrollToSection('introduction')}>
+            Juvens.dev
+          </h1>
           <div className='hidden sm:flex text-white'>
             <ul className='flex lg:gap-0 md:gap-4 sm:gap-3 gap-2 xl:text-lg md:text-base sm:text-sm text-xs font-[500] lg:bg-white/20 lg:backdrop-blur-md lg:rounded-4xl'>
               <li className='cursor-pointer lg:rounded-l-full lg:hover:bg-white/10 lg:py-2 lg:px-5 transition duration-200' onClick={() => scrollToSection('introduction')}>About</li>
@@ -39,7 +41,6 @@ const Header = () => {
           <a href={encodeURI("/CV - Juvens Laurensius Dwitama.pdf")}
             target="_blank"
             rel="noopener noreferrer"
-            download="Juvens-Resume.pdf"
             className='bg-white/20 backdrop-blur-md rounded-md cursor-pointer lg:block hidden'
           >
             <div className="flex items-center justify-center gap-2 hover:bg-white/15 rounded-md py-2 px-5 transition duration-200">
@@ -58,14 +59,14 @@ const Header = () => {
       </div>
 
       {isMenuOpen && (
-        <div className='sm:hidden fixed top-[70px] left-0 right-0 z-40 flex justify-center'>
-          <ul className='flex flex-col w-full max-w-[308px] bg-white/10 backdrop-blur-md rounded-lg text-white font-[500] text-sm overflow-hidden'>
+        <div className='sm:hidden fixed top-[60px] left-0 right-0 z-40 flex justify-center'>
+          <ul className='flex flex-col items-center w-full max-w-[302px] bg-white/10 backdrop-blur-sm rounded-lg text-white font-[500] text-sm overflow-hidden'>
             <li className='cursor-pointer hover:bg-white/10 py-3 px-5 transition duration-200' onClick={() => scrollToSection('introduction')}>About</li>
-            <li className='border-t border-white/15'></li>
+            <li className='border-t border-white/30 w-full'></li>
             <li className='cursor-pointer hover:bg-white/10 py-3 px-5 transition duration-200' onClick={() => scrollToSection('experiences')}>Experiences</li>
-            <li className='border-t border-white/15'></li>
+            <li className='border-t border-white/30 w-full'></li>
             <li className='cursor-pointer hover:bg-white/10 py-3 px-5 transition duration-200' onClick={() => scrollToSection('projects')}>Projects</li>
-            <li className='border-t border-white/15'></li>
+            <li className='border-t border-white/30 w-full'></li>
             <li className='cursor-pointer hover:bg-white/10 py-3 px-5 transition duration-200' onClick={() => scrollToSection('skills')}>Skills</li>
           </ul>
         </div>
