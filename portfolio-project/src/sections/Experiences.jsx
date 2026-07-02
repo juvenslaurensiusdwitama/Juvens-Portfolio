1ea2d52affcd6344348c67b63af1b09467c3f1f2
juvenslaurensiusdwitama <juvenslaurensiusdwitama@gmail.com>
2026-07-02 14:35:11 +0700

    '.'

--- portfolio-project/src/sections/Experiences.jsx
+++ portfolio-project/src/sections/Experiences.jsx
@@ -1,41 +1,7 @@
 import Experience from '../components/Experience'
-import ramu from '../assets/ramu.png'
-import alterra from '../assets/alterra.png'
-import schooltech from '../assets/schooltech.png'
+import { experienceDatas } from './Data/experienceDatas'
 
 const Experiences = () => {
-  const datas = [
-    {
-      image: ramu,
-      company: 'Ramu Studio',
-      position: 'Front-End Web Developer',
-      type: 'Freelance',
-      startDate: 'Jan 2025',
-      endDate: 'Feb 2025',
-      descriptions: 'Developed a custom booking system for BMusicStudio that streamlined studio reservations, improved customer experience, and boosted operational efficiency. I also developed and designed a professional company profile for PT GIAT (Global Intelligence Artificial Technology) that established a clear brand identity and delivered an engaging presentation to clients and stakeholders.',
-      delay: 300,
-    },
-    {
-      image: schooltech,
-      company: 'SchoolTech Indonesia',
-      position: 'IT Front-End Developer',
-      type: 'Internship',
-      startDate: 'Aug 2024',
-      endDate: 'Dec 2024',
-      descriptions: 'Collaborated with product managers and backend developers to implement core features such as role, partner, mentor, and activity management, while integrating RESTful APIs to deliver real-time data across student, teacher, and admin dashboards. I also worked with the UI/UX division to ensure a user-friendly interface, and built responsive web features for a school management platform using modern front-end tools. In an Agile/Scrum environment, I took part in code reviews, QA testing, and sprint planning, and consistently addressed bug fixes and feature improvements based on supervisor feedback.',
-      delay: 300,
-    },
-    {
-      image: alterra,
-      company: 'Alterra Academy',
-      position: 'Front-End Engineer With ReactJS',
-      type: 'SIB Kampus Merdeka',
-      startDate: 'Feb 2024',
-      endDate: 'Jun 2024',
-      descriptions: 'As the lead of the Front-end/React division for the capstone project, I developed the admin dashboard along with the manage products and product measurement data features, while collaborating closely with other divisions. In my personal projects, I built user authentication and authorization, implemented RESTful API for comments, and developed search, save, and AI chatbot features.',
-      delay: 400,
-    },
-  ]
   return (
     <div
       className='relative flex flex-col items-center sm:gap-6 gap-4 text-white lg:pt-23 sm:pt-18 pt-4.5'
@@ -43,7 +9,7 @@ const Experiences = () => {
     >
       <h1 className='xl:text-3xl lg:text-2xl sm:text-xl text-lg font-semibold' data-aos="fade-left" data-aos-duration={1000}>Experiences</h1>
       <div className='flex flex-col xl:gap-5 md:gap-4.5 gap-4 justify-center items-center w-full xl:max-w-[1300px] xl:px-[50px] lg:max-w-[910px] md:max-w-[700px] sm:max-w-[580px] max-w-[302px]'>
-        {datas.map((data, i) => (
+        {experienceDatas.map((data, i) => (
           <Experience key={i} data={data} />
         ))}
       </div>
--- portfolio-project/src/sections/Skills.jsx
+++ portfolio-project/src/sections/Skills.jsx
@@ -1,114 +1,14 @@
 import Skill from '@/components/Skill'
-import html from '../assets/html.png'
-import css from '../assets/css.png'
-import js from '../assets/js.png'
-import react from '../assets/react.png'
-import tailwind from '../assets/tailwind.png'
-import ts from '../assets/typescript.png'
-import nextjs from '../assets/next.png'
-import firebase from '../assets/firebase.png'
-import postman from '../assets/postman.png'
-import git from '../assets/git.png'
-import github3 from '../assets/github3.png'
-import vite from '../assets/vite.png'
-import vercel from '../assets/vercel.png'
-import figma from '../assets/figma.png'
+import { skillDatas } from './Data/SkillDatas'
 
 const Skills = () => {
-  const datas = [
-    {
-      icon: html,
-      name: 'HTML',
-      type: 'Markup Language',
-      delay: 200,
-    },
-    {
-      icon: css,
-      name: 'CSS',
-      type: 'Style Sheet Language',
-      delay: 300,
-    },
-    {
-      icon: js,
-      name: 'JavaScript',
-      type: 'Programming Language',
-      delay: 400,
-    },
-    {
-      icon: react,
-      name: 'React JS',
-      type: 'Library',
-      delay: 500,
-    },
-    {
-      icon: tailwind,
-      name: 'Tailwind CSS',
-      type: 'Framework',
-      delay: 600,
-    },
-    {
-      icon: ts,
-      name: 'TypeScript',
-      type: 'Programming Language',
-      delay: 700,
-    },
-    {
-      icon: nextjs,
-      name: 'Next JS',
-      type: 'Framework',
-      delay: 800,
-    },
-    {
-      icon: git,
-      name: 'GIT',
-      type: 'Version Control System',
-      delay: 900,
-    },
-    {
-      icon: github3,
-      name: 'GitHub',
-      type: 'Version Control Platform',
-      delay: 1000,
-    },
-    {
-      icon: firebase,
-      name: 'Firebase',
-      type: 'Backend Service',
-      delay: 1100,
-    },
-    {
-      icon: postman,
-      name: 'Postman API',
-      type: 'Testing Tool',
-      delay: 1200,
-    },
-    {
-      icon: vite,
-      name: 'Vite',
-      type: 'Build Tool',
-      delay: 1300,
-    },
-    {
-      icon: vercel,
-      name: 'Vercel',
-      type: 'Deployment Platform',
-      delay: 1400,
-    },
-    {
-      icon: figma,
-      name: 'Figma',
-      type: 'Design Tool',
-      delay: 1500,
-    },
-  ]
-
   return (
     <div
       className='flex flex-col items-center sm:gap-6 gap-4 min-h-screen bg-center bg-cover lg:pt-23 sm:pt-18 pt-4.5 pb-10' id='skills'
     >
       <h1 data-aos="fade-left" data-aos-duration={1000} className='text-center text-white xl:text-3xl lg:text-2xl sm:text-xl text-lg font-semibold'>My Skills</h1>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full xl:max-w-[1300px] xl:px-[50px] lg:max-w-[910px] md:max-w-[700px] sm:max-w-[580px] max-w-[302px] justify-center'>
-        {datas.map((data, index) => (
+        {skillDatas.map((data, index) => (
           <Skill key={index} data={data} />
         ))}
       </div>
